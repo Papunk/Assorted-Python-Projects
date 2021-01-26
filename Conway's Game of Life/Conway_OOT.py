8ac8a227b9c985e36f69eca8001e17dd47a912d9
Pedro <pedrocpt2001@gmail.com>
2021-01-26 09:09:32 -0400

    Made skeleton for conway's life with an object oriented approach

--- Conway's Game of Life/Conway_OOT.py
+++ Conway's Game of Life/Conway_OOT.py
@@ -1,11 +1,42 @@
-ALIVE = " ⬢ "
-DEAD = "   "
+import random
+import time
+
+
 
 class Being:
-    kind = ""
+
+    ALIVE = " ⬢ "
+    DEAD = "   "
+
+    def __init__(self, kind=Being.DEAD):
+        self.kind = kind
+
+    def isDead(self):
+        return self.kind == Being.DEAD
+
 
 class GameMatrix:
     matrix = []
 
+    def __init__(self, ySize=0, xSize=0):
+        row = []
+        for _ in range(ySize):
+            row = []
+            for _ in range(xSize):
+                row.append(Being())
+            self.matrix.append(row)
+
+
 class Game:
-    pass
+    
+    @staticmethod
+    def printState():
+        pass
+
+    @staticmethod
+    def updateState():
+        pass
+
+    @staticmethod
+    def run():
+        pass
