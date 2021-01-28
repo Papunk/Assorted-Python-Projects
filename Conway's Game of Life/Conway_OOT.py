6e2e354b1218074e0cd3798f3588a7afc4e371c8
Pedro <pedrocpt2001@gmail.com>
2021-01-28 08:44:25 -0400

    Finished OO approach to conway's life; missing run() method

--- Conway's Game of Life/Conway_OOT.py
+++ Conway's Game of Life/Conway_OOT.py
@@ -13,6 +13,15 @@ class Being:
     def isDead(self):
         return self.kind == Being.DEAD
 
+    def isAlive(self):
+        return self.kind == Being.ALIVE
+
+    def kill(self):
+        self.kind = Being.DEAD
+    
+    def revive(self):
+        self.kind = Being.ALIVE
+
 
 
 class GameMatrix:
@@ -46,11 +55,39 @@ class Game:
 
     def updateState(self):
         newMatrix = self.gameMatrix.matrix
-        for yCoord in range(self.gameMatrix.ySize):
-            for xCoord in range(self.gameMatrix.ySize):
+        x = self.gameMatrix.xSize
+        y = self.gameMatrix.ySize
+        for yCoord in range(y):
+            for xCoord in range(x):
 
-        #         
-        
+                cell = newMatrix[yCoord][xCoord]
+                surroundingCellTally = 0
+                
+                surroundingCellTally += 1 if newMatrix[yCoord - 1][xCoord].isAlive() else 0
+                if yCoord != y - 1:
+                    surroundingCellTally += 1 if newMatrix[yCoord + 1][xCoord].isAlive() else 0
+                if xCoord != 0:
+                    surroundingCellTally += 1 if newMatrix[yCoord][xCoord - 1].isAlive() else 0
+                if xCoord != x - 1:
+                    surroundingCellTally += 1 if newMatrix[yCoord][xCoord + 1].isAlive() else 0
+
+                if yCoord != 0 and xCoord != 0:
+                    surroundingCellTally += 1 if newMatrix[yCoord - 1][xCoord - 1].isAlive() else 0
+                if yCoord != 0 and xCoord != x - 1:
+                    surroundingCellTally += 1 if newMatrix[yCoord - 1][xCoord + 1].isAlive() else 0
+                if yCoord != y - 1 and xCoord != 0:
+                    surroundingCellTally += 1 if newMatrix[yCoord + 1][xCoord - 1].isAlive() else 0
+                if yCoord != y - 1 and xCoord != x - 1:
+                    surroundingCellTally += 1 if newMatrix[yCoord + 1][xCoord + 1].isAlive() else 0
+                
+                if cell.isAlive():
+                    if surroundingCellTally < 2 or surroundingCellTally > 3:
+                        newMatrix[yCoord][xCoord].kill()
+                elif cell.isDead():
+                    if surroundingCellTally == 3:
+                        newMatrix[yCoord][xCoord].revive()
+        return newMatrix
+
 
     def run(self):
         pass
