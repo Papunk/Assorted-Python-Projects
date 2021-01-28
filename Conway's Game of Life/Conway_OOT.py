bcbec516a6826d5b61d58ec826f6dfce8c8e97bf
Pedro <pedrocpt2001@gmail.com>
2021-01-27 23:13:56 -0400

    worked on oop

--- Conway's Game of Life/Conway_OOT.py
+++ Conway's Game of Life/Conway_OOT.py
@@ -2,7 +2,6 @@ import random
 import time
 
 
-
 class Being:
 
     ALIVE = " ⬢ "
@@ -15,11 +14,15 @@ class Being:
         return self.kind == Being.DEAD
 
 
+
 class GameMatrix:
+
     matrix = []
 
     def __init__(self, ySize=0, xSize=0):
         row = []
+        self.ySize = ySize
+        self.xSize = xSize
         for _ in range(ySize):
             row = []
             for _ in range(xSize):
@@ -27,16 +30,27 @@ class GameMatrix:
             self.matrix.append(row)
 
 
+
 class Game:
     
-    @staticmethod
-    def printState():
-        pass
+    def __init__(self, ySize=0, xSize=0):
+        self.gameMatrix = GameMatrix(ySize, xSize)
+        
 
-    @staticmethod
-    def updateState():
-        pass
+    def printState(self):
+        for row in self.gameMatrix.matrix:
+            for cell in row:
+                print(cell.kind, end="")
+            print()
 
-    @staticmethod
-    def run():
+
+    def updateState(self):
+        newMatrix = self.gameMatrix.matrix
+        for yCoord in range(self.gameMatrix.ySize):
+            for xCoord in range(self.gameMatrix.ySize):
+
+        #         
+        
+
+    def run(self):
         pass
